adc756cd0aff4dbb3401e3b38c55dc4ecf19b006
Akash-Vijayakuamar-001 <akash2023b@gmail.com>
2026-01-22 18:51:30 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -1,1 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DBConnection {
+
+    // Change these credentials based on your MySQL setup
+    private static final String URL = "jdbc:mysql://localhost:3306/student_db";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+}
